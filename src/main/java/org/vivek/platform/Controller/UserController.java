a3861418870333799c1fe03b2bedf763f3c89068
Lenovo <vivekmane9860@gmail.com>
2025-06-09 19:00:33 +0530

    controller creation

---
+++ src/main/java/org/vivek/platform/Controller/UserController.java
@@ -0,0 +1,9 @@
+package org.vivek.platform.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+}
