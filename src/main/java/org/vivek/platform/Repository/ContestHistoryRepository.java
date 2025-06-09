911ff1470fe8a9d927556f9ad9c51ab7b22e24cc
Lenovo <vivekmane9860@gmail.com>
2025-06-09 13:33:48 +0530

    repository creation for storing leetcode ranking and rating history

---
+++ src/main/java/org/vivek/platform/Repository/ContestHistoryRepository.java
@@ -0,0 +1,10 @@
+package org.vivek.platform.Repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.vivek.platform.Model.Leetcode.Ratings.RankingInfo;
+import org.vivek.platform.Model.Leetcode.Ratings.UserContestRankingHistory;
+
+@Repository
+public interface ContestHistoryRepository extends JpaRepository<UserContestRankingHistory, Long> {}
