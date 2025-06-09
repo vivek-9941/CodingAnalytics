a44da4ed1b850191cd187b797101970017df5502
Lenovo <vivekmane9860@gmail.com>
2025-06-09 20:08:19 +0530

    codechef controller

--- src/main/java/org/vivek/platform/Controller/CodechefController.java
+++ src/main/java/org/vivek/platform/Controller/CodechefController.java
@@ -1,9 +1,23 @@
 package org.vivek.platform.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.vivek.platform.Model.Codechef.Codechef;
+import org.vivek.platform.Model.User;
+import org.vivek.platform.Service.CodechefService;
 
 @RestController
 @RequestMapping("/api/codechef")
 public class CodechefController {
+    @Autowired
+    CodechefService codechefService;
+    public ResponseEntity<?> getCodechef(String username, User user) {
+        Codechef result =  codechefService.getCodechef(username, user);
+        if(result == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
 }
