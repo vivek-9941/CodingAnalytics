b41010c00159e9b9ff9a6d3acb64ed78fbba5ac9
Lenovo <vivekmane9860@gmail.com>
2025-06-10 12:11:44 +0530

    CustomOAuth2SuccessHandler

---
+++ src/main/java/org/vivek/platform/Security/CustomOAuth2SuccessHandler.java
@@ -0,0 +1,28 @@
+package org.vivek.platform.Security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler {
+    @Autowired
+    private JwtService jwtService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        String email = oAuth2User.getAttribute("email");
+
+        String token = jwtService.generateToken(email);
+
+        response.sendRedirect("http://localhost:5173/oauth-success?token=" + token);
+    }
+}
