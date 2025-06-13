5ff43fbe9e85c61ff09b25b677631236ceaac739
Lenovo <vivekmane9860@gmail.com>
2025-06-13 14:33:16 +0530

    updated the controller for the accepting the user object and handle as parameter

--- src/main/java/org/vivek/platform/Controller/CodechefController.java
+++ src/main/java/org/vivek/platform/Controller/CodechefController.java
@@ -2,22 +2,21 @@ package org.vivek.platform.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.vivek.platform.Model.Codechef.Codechef;
 import org.vivek.platform.Model.User;
 import org.vivek.platform.Service.CodechefService;
 
 @RestController
 @RequestMapping("/api/codechef")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class CodechefController {
     @Autowired
     CodechefService codechefService;
 
-    @PostMapping("fetch")
-    public ResponseEntity<?> getCodechef(String username, User user) {
-        Codechef result =  codechefService.getCodechef(username, user);
+    @PostMapping("/get")
+    public ResponseEntity<?> getCodechef(@RequestParam String handle, @RequestBody User user) {
+        Codechef result =  codechefService.getCodechef(handle, user);
         if(result == null) {
             return ResponseEntity.notFound().build();
         }
