a27e927d2a4b4c659ef405a0a15bc0d9492aec84
Lenovo <vivekmane9860@gmail.com>
2025-06-20 16:48:43 +0530

    removed the persistence (Repo layer) for the submission stats

--- src/main/java/org/vivek/platform/Model/Leetcode/Ratings/RankingInfo.java
+++ src/main/java/org/vivek/platform/Model/Leetcode/Ratings/RankingInfo.java
@@ -8,25 +8,19 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "user_contest_ranking_info")
 public class RankingInfo {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+
     private String username;
     private Integer attendedContestsCount;
     private Double rating;
     private Integer globalRanking;
     private Double topPercentage;
     private LocalDateTime lastupdated;
-
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-    @JoinColumn(name = "user_ranking_info_id")  // This creates the foreign key in the child table
     private List<UserContestRankingHistory> contestHistory = new ArrayList<>();
 
 }
