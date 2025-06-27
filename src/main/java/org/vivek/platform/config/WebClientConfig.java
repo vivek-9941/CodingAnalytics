f4c04be6907e0881bf09bb6865f349b3ed380dfd
Lenovo <vivekmane9860@gmail.com>
2025-06-27 16:38:24 +0530

    main interceptor

--- src/main/java/org/vivek/platform/config/WebClientConfig.java
+++ src/main/java/org/vivek/platform/config/WebClientConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class WebConfig {
+public class WebClientConfig {
     @Bean
     public WebClient CodeforcesClient()
     {
